709fb53fd97e6ffb28293192cbbb05c0fc948d11
MeGallin <me@garyallin.uk>
2024-12-29 15:40:24 +0000

    Delete message working

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   createMessage,
   getMessages,
+  deleteMessage,
 } = require('../controllers/messageController');
 
 router.post('/create-message', createMessage);
 router.get('/:chatId', getMessages);
+router.delete('/:messageId', deleteMessage);
 module.exports = router;
